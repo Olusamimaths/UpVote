e510b9cc65de8d012b51a70f64e629c22baf8d22
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2020-04-29 16:05:44 +0100

    Complete project files updated

--- docs/seed.js
+++ docs/seed.js
@@ -6,8 +6,8 @@ window.Seed = (function () {
       description: 'On-demand sand castle construction expertise.',
       url: '#',
       votes: 16,
-      avatar: '../assets/images/avatars/daniel.jpg',
-      submissionImage: '../assets/images/submissions/image-yellow.png',
+      avatar: 'assets/images/avatars/daniel.jpg',
+      submissionImage: 'assets/images/submissions/image-yellow.png',
     },
     {
       id: 2,
@@ -15,8 +15,8 @@ window.Seed = (function () {
       description: 'Earn points when your favorite politicians pass legislation.',
       url: '#',
       votes: 11,
-      avatar: '../assets/images/avatars/kristy.png',
-      submissionImage: '../assets/images/submissions/image-rose.png',
+      avatar: 'assets/images/avatars/kristy.png',
+      submissionImage: 'assets/images/submissions/image-rose.png',
     },
     {
       id: 3,
@@ -24,8 +24,8 @@ window.Seed = (function () {
       description: 'We have your measurements and shipping address.',
       url: '#',
       votes: 17,
-      avatar: '../assets/images/avatars/veronika.jpg',
-      submissionImage: '../assets/images/submissions/image-steel.png',
+      avatar: 'assets/images/avatars/veronika.jpg',
+      submissionImage: 'assets/images/submissions/image-steel.png',
     },
     {
       id: 4,
@@ -33,8 +33,8 @@ window.Seed = (function () {
       description: 'High-minded or absent-minded? You decide.',
       url: '#',
       votes: 9,
-      avatar: '../assets/images/avatars/molly.png',
-      submissionImage: '../assets/images/submissions/image-aqua.png',
+      avatar: 'assets/images/avatars/molly.png',
+      submissionImage: 'assets/images/submissions/image-aqua.png',
     }
   ];
 
